8bb5c8fc5586847caef2aa95dcda44df415e61de
RevizardTV <168398688+RevizardTV@users.noreply.github.com>
2026-02-23 19:37:12 +0530

    minor bug fix

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ const insertNumber=(sentNumber)=>{
     console.log("insertNumber called:",displayNum);
 }
 
-const operationEnterNumbers(op)=>{
+const operationEnterNumbers=(op)=>{
     const currentString=displayNum.toString();
     if(op==='-' && storedNums==="")
     {
